d6189bed85ce1d0275c0d16b8a9d66e3bc50e3d3
step <step1116kil@gmail.com>
2025-08-16 00:19:45 +0300

    Fixed statuses v2

--- app/database/requests.py
+++ app/database/requests.py
@@ -24,6 +24,8 @@ class User:
 
 
 class Order:
+    _PAYMENT_IS_REQUIRED_TEXT = "Ожидается оплата🌀"
+
     def __init__(self, id: Optional[int] = None):
         self.id = id
     
@@ -54,12 +56,20 @@ class Order:
     
     async def get_order(self) -> Dict[str, Any]:
         if self.id:
-            return await OrderTable.filter(id=self.id).first().values()
+            order = await OrderTable.filter(id=self.id).first().values()
+            if order["status"] == "new":
+                order["status"] = self._PAYMENT_IS_REQUIRED_TEXT
+            return order
         return {}
     
-    @staticmethod
-    async def get_user_orders(user_id: int) -> List[Dict[str, Any]]:
-        return await OrderTable.filter(status="new", mammoth_id=user_id).values()
+    async def get_user_orders(self, user_id: int) -> List[Dict[str, Any]]:
+        orders = await OrderTable.filter(status="new", mammoth_id=user_id).values()
+        new_orders = []
+        for order in orders:
+            if order["status"] == "new":
+                order["status"] = self._PAYMENT_IS_REQUIRED_TEXT
+            new_orders.append(order)
+        return new_orders
     
     async def delete(self) -> None:
         await OrderTable.filter(id=self.id).update(status="cancelled")
